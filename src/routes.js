11eb59c7fa8f47ff6daf54e37359a2d076ea59e6
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-05-25 18:23:52 -0300

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
 routes.get('/', RecipientController.store);
+routes.post('/users', UserController.store);
 
 export default routes;
