d46b7178cd13a2131bfa47369eaef03a9a78bc85
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-06-02 17:42:47 -0300

    Update RecipientController.js

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required,
+      street: Yup.string().required,
+      number: Yup.string().required,
+      complement: Yup.string(),
+      state: Yup.string().required,
+      city: Yup.string().required,
+      zip_code: Yup.string().required,
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const recipientExists = await Recipient.findOne({
       where: { name: req.body.name },
     });
@@ -20,7 +35,6 @@ class RecipientController {
       zip_code,
     } = Recipient.create(req.body);
 
-    console.log(name, street);
     return res.json({
       message: 'ok',
       name,
@@ -31,6 +45,29 @@ class RecipientController {
       city,
       zip_code,
     });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().positive().required(),
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      zip_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+    const { id } = req.body;
+    const recipient = await Recipient.findByPk(id);
+
+    const { name, street } = await recipient.update(req.body);
+
+    return res.json({ message: 'Recipient updated', recipient });
   }
 }
 
