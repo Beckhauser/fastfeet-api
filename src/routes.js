2205ad260764d1e215b5692116768cfb4ae1c6e0
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-06-02 17:41:05 -0300

    Add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+
+import authMiddleware  from './app/middlewares/auth';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
@@ -6,7 +8,11 @@ import UserController from './app/controllers/UserController';
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 routes.get('/', RecipientController.store);
+routes.put('/recipients', RecipientController.update);
+
 routes.post('/users', UserController.store);
 
 export default routes;
