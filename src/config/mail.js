87feb7e15f033c93186be124f7f778719c6e1832
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-08-17 20:47:21 -0300

    Add .env

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '97fc29aa875185',
-    pass: '5d3d76957872aa',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equiper notion <noreply@fortest.com>',
