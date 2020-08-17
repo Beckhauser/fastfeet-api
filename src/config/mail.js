a4620a2b53918848fc4be69e934237f55b844da0
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-08-17 17:12:19 -0300

    Create mail.js

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '97fc29aa875185',
+    pass: '5d3d76957872aa',
+  },
+  default: {
+    from: 'Equiper notion <noreply@fortest.com>',
+  },
+};
