eb03067d3f9d84d865741e5144315a16777a5995
Lucas Alexandre Beckhauser <38123030+Beckhauser@users.noreply.github.com>
2020-08-17 17:50:21 -0300

    :green_book:  Create NewDeliveryMail to use with bee-queue

---
+++ src/app/jobs/NewDeliveryMail.js
@@ -0,0 +1,24 @@
+import Mail from '../../lib/Mail';
+
+class NewDeliveryMail {
+  get key() {
+    return 'NewDeliveryMail';
+  }
+
+  async handle({ data }) {
+    const { delivery } = data;
+
+    await Mail.sendMail({
+      to: `${delivery.deliveryman.name} <${delivery.deliveryman.email}>`,
+      subject: 'Nova entrega',
+      template: 'delivery',
+      context: {
+        deliveryman: delivery.deliveryman.name,
+        recipient: delivery.recipient.name,
+        product: delivery.product,
+      },
+    });
+  }
+}
+
+export default new NewDeliveryMail();
